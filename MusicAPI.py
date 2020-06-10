c28b397014d4d3103bfcd11389a6062fae9a663c
eva bernal <5.22.1@hotmail.ca>
2020-06-10 18:14:06 -0400

    Saved song info from Google Play Music to text file

---
+++ MusicAPI.py
@@ -0,0 +1,16 @@
+from gmusicapi import Mobileclient
+import json
+import os
+
+api = Mobileclient()
+#api.perform_oauth()
+
+device_id = os.getenv("DEVICE_ID")
+api.oauth_login(device_id)
+
+library = api.get_all_songs()
+library_str = json.dumps(library, indent=4)
+
+f = open("./songs.txt", "w")
+f.write(library_str)
+f.close()
