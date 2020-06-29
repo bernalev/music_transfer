2903d51eadf1b1d8874eab52d342cf80f73aa1b3
Eva Bernal <e.bernal@mail.utoronto.ca>
2020-06-29 13:23:14 -0400

    cleaned up SortMusic.py

--- Sorting/SortMusic.py
+++ Sorting/SortMusic.py
@@ -5,40 +5,27 @@ import pyreadline
 import LibraryManager
 import PlaylistManager
 
-f = open("../Google Play Music/all_songs.txt", "r")
-library = json.loads(f.read())
-f.close()
-library = LibraryManager.LibraryManager(library)
-
-artists = iter(library.get_artists())
-
-f = open('sorted_songs.txt', 'r')
-playlists = json.loads(f.read())
-f.close()
-plsts = PlaylistManager.PlaylistManager(playlists)
-
-
 class Command(cmd.Cmd):
     prompt = '> '
 
     def complete_sendto(self, text, line, start_index, end_index):
-        return plsts.starts_with(text)
+        return playlists.starts_with(text)
 
     def do_sendto(self, line):
-        if not plsts.has_playlist(line):
+        if not playlists.has_playlist(line):
             return
 
         artist_songs = library.remove_artist(self.artist)
-        plsts.add_to_playlist(line, self.artist, artist_songs)
+        playlists.add_to_playlist(line, self.artist, artist_songs)
 
     def do_info(self, line):
         library.print_artist(self.artist)
 
     def do_make(self, line):
-        plsts.add_playlist(name=line)
+        playlists.add_playlist(name=line)
 
     def do_playlists(self, line):
-        plsts.print_playlists()
+        playlists.print_playlists()
 
     def do_next(self, line):
         try:
@@ -49,13 +36,7 @@ class Command(cmd.Cmd):
             return True
 
     def do_exit(self, line):
-        f = open('sorted_songs.txt', 'w')
-        f.write(json.dumps(plsts.playlists, indent=4))
-        f.close()
-
-        f = open('unsorted_songs.txt', 'w')
-        f.write(json.dumps(library.get_library(), indent=4))
-        f.close()
+        save_files()
         return True
 
     def default(self, line):
@@ -71,6 +52,31 @@ class Command(cmd.Cmd):
         else:
             print('command does not exist.')
 
+
+def save_files():
+    file = open('sorted_songs.txt', 'w')
+    file.write(json.dumps(playlists.get_playlists(), indent=4))
+    file.close()
+
+    file = open('unsorted_songs.txt', 'w')
+    file.write(json.dumps(library.get_library(), indent=4))
+    file.close()
+
+def open_files():
+    file = open("../Google Play Music/all_songs.txt", "r")
+    library = json.loads(file.read())
+    file.close()
+
+    file = open('sorted_songs.txt', 'r')
+    playlists = json.loads(file.read())
+    file.close()
+
+    return library, playlists
+
+library, playlists = open_files()
+library = LibraryManager.LibraryManager(library)
+playlists = PlaylistManager.PlaylistManager(playlists)
+artists = iter(library.get_artists())
 
 cmd = Command()
 cmd.cmdloop()
