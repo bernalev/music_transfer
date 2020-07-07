0c234e06917ebad7b3639956b960c659810b798f
Eva Bernal <e.bernal@mail.utoronto.ca>
2020-07-07 15:09:41 -0400

    gave iterator standard methods

--- Sorting/Iterator.py
+++ Sorting/Iterator.py
@@ -1,4 +1,26 @@
-
 class Iterator:
-    def __init__():
+    def __init__(items):
+        self.index = 0
+        self.items = []
+
+    def has_next():
+        return self.index < self.items.length - 1
+
+    def next():
+        if self.hasNext():
+           self.index += 1
+        return self.current()
+
+    def songs():
         pass
+
+    def prev():
+        if self.index != 0:
+            self.index -= 1
+        return self.current()
+
+    def current():
+        return self.items[self.index]
+
+    
+
